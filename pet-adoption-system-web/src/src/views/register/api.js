cefd2735bbe48445730446ba7e123e27234bf6ea
LeiMeiLing <130131325+LeiMeiLing@users.noreply.github.com>
2023-04-10 10:20:52 +0800

    Delete pet-adoption-system-web/src/src directory

--- pet-adoption-system-web/src/src/views/register/api.js
+++
@@ -1,8 +0,0 @@
-import http from "../../config/http";
-export function register(userInfo){
-    return http.put("/user/register", {
-        ...userInfo
-    }).then(res=>{
-        return res;
-    })
-}
